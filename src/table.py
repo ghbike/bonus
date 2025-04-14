1a1db6c5be7bd49fb89375c1da41d517c08f4869
sbike <mysilverbike@gmail.com>
2025-04-14 12:46:50 +0400

    table: count combinations, time, bets, float, jp, % magic and mistery are included

--- src/table.py
+++ src/table.py
@@ -14,53 +14,65 @@ class Table:
     increment = .015
     min_bet = 5
     max_bet = 25
-    bets = (5, 5, 5, 5, 10, 10, 10, 15, 20, 25)
+    bets = (5, 5, 5, 5, 5, 5, 10, 15, 20, 25)  # average = 10
+    bets_total = 0
 
     def __init__(self):
         self.deck = Deck()
-        self.payout_table = {"Diamond Royal Flush": 0, "Royal Flush": 350,
+        self.payouts = {"Diamond Royal Flush": 0, "Royal Flush": 350,
             "Straight Flush": 200, "4oak": 300, "Full house": 100,
             "Flush": 75, "Straight": 50, "3oak": 10, "No game": 0}
         self.jp_payouts = {"Diamond Royal Flush": 1.00, "Royal Flush": .25,
             "Straight Flush": .20, "4oak": 0, "Full house": 0,
             "Flush": 0, "Straight": 0, "3oak": 0, "No game": 0}
-        self.increment_table = {"Diamond Royal Flush": 0, "Royal Flush": 0,
-            "Straight Flush": 0, "4oak": 0, "Full house": 0,
-            "Flush": 0, "Straight": 0, "3oak": 0, "No game": self.increment}
-        self.profit_table = {"Diamond Royal Flush": 0, "Royal Flush": 0,
-                                "Straight Flush": 0, "4oak": 0, "Full house": 0,
-                                "Flush": 0, "Straight": 0, "3oak": 0, "No game": 1}
+        self.magic = {"Magic Card": (1 / 50, 8), "Mistery p/o": (1 / 1_000, 10)}
 
     def __str__(self):
-        return f"float: {self.table_float}, JP: {self.jp}"
+        return f"float: {self.table_float}, JP: {round(self.jp, 2)}"
 
     def one_hand_dealing(self):
+        # ------------------------------- Dealing ---------------------------------
+        bet = random.choice(self.bets)
+        self.bets_total += bet
         hand = Hand(self.deck)
         hand_game = hand.hand_is()
-        bet = random.choice(self.bets)
         self.cnt_dct[hand_game] += 1
-
-        self.table_float += self.profit_table[hand_game] * bet  # to float
-        self.table_float -= self.payout_table[hand_game] * bet  # from float
-        self.jp += self.increment_table[hand_game] * bet        # to jp
-        self.jp -= round(self.jp * self.jp_payouts[hand_game] * bet/self.max_bet, 0) # from jp
-        if self.jp < self.jp_start:  #increasing jp upto jp_start using float
-            self.table_float += round(self.jp - self.jp_start, 0)
-            self.jp = self.jp_start
-
+        # ------------------------------- Magic -----------------------------------
+        magic_payment = 0
+        mistery_payment = 0
+        if random.random() < self.magic["Magic Card"][0]:
+            magic_card = random.choice(self.deck.cards)
+            if magic_card in hand.cards:
+                magic_payment = self.magic["Magic Card"][1]
+        if random.random() < self.magic["Mistery p/o"][0]:
+            mistery_payment = self.magic["Mistery p/o"][1]
+        # --------------------------------payouts-----------------------------------
+        payout = self.payouts[hand_game] * bet
+        magic_payout = (magic_payment + mistery_payment) * bet
+        jp_payout = round(self.jp_payouts[hand_game] * self.jp, 0)
+        if not (payout + magic_payout + jp_payout):      # if bonus bet loses
+            float_profit = bet
+            jp_profit = bet * self.increment
+            self.table_float += float_profit
+            self.jp += jp_profit
+        else:                                            # if bonus bet wins
+            self.table_float -= payout + magic_payout + jp_payout
+            self.jp -= jp_payout
+        if self.jp < self.jp_start: self.jp = self.jp_start    # jp auto refill
+        return bet
 
 if __name__ == '__main__':
 
 
+    bets = 0
     table = Table()
-    for _ in range(10):
+    for _ in range(5):
         time_start = time()
-        for i in range(2_598_960):
+        for i in range(25_989_600):
             table.one_hand_dealing()
-
         time_end = time()
         total_time = time_end - time_start
-        print(f'time: {round(total_time, 0)}')
+        print(f'time: {round(total_time, 0)}  #: {_}  bets: {table.bets_total}')
         print(table.cnt_dct)
-        print(table)
+        print(table, 100 * table.table_float / table.bets_total)
         print("-----------------------------")
